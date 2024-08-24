d3630a5e56d81a3a55ea25dcf0c0e3fa1e8286f7
RahafDuhaimesh <rahafduhaimesh211@gmail.com>
2024-08-24 23:12:05 +0300

    Task 2 - Query string and Route

---
+++ 24-8-2024-Task2/24-8-2024-Task2/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using _24_8_2024_Task2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _24_8_2024_Task2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+        public UsersController(MyDbContext db) { _db = db; }
+
+        [HttpGet]
+        [Route("Users/All")]
+        public IActionResult getAllUsers()
+        {
+            var x = _db.Users.ToList();
+            if (x == null) { return NotFound(); }
+            return Ok(x);
+        }
+
+        [Route("Users/{id:int}")]
+        [HttpGet]
+
+        public IActionResult GetUserById(int id)
+        {
+            if (id <= 0) { return BadRequest(); }
+
+            var x = _db.Users.FirstOrDefault(d => d.Id == id);
+            if (x == null)
+            {
+                return NotFound();
+            }
+            return Ok(x);
+        }
+
+        [Route("Users/{Name}")]
+        [HttpGet]
+        public IActionResult GetUsersByName(string Name)
+        {
+            if (Name == null) { return BadRequest(); }
+            var y = _db.Users.FirstOrDefault(x => x.UserName == Name);
+            if (y == null)
+            {
+                return NotFound();
+            }
+            return Ok(y);
+        }
+
+        [Route("Users/{id:int}")]
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+            if (id <= 0) { return BadRequest(); }
+            var x = _db.Users.FirstOrDefault(f => f.Id == id);
+            if (x == null) { return NotFound(); }
+            _db.Users.Remove(x);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
